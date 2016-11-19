8012eac542249c36fe4d05bf3fa9d3f411c51d07
Chris Illingworth <christo743@hotmail.co.uk>
2016-11-19 14:01:02 -0700

    Implemented Player detail page and added awards list to it

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -4,4 +4,15 @@ class AccessController < ApplicationController
     @players = Player.order(score: :desc).first(20)
   end
 
+  def player
+
+    if params[:id].nil?
+      params[:id] = 1
+    end
+
+    @player = Player.find(params[:id])
+
+
+  end
+
 end
