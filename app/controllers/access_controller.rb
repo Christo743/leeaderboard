2161e7c025ad1604e0e0b4ee5fb275ea86473adb
Chris Illingworth <christo743@hotmail.co.uk>
2016-11-29 14:18:07 -0700

    Weekly highscore added to the home page

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,10 @@ class AccessController < ApplicationController
 
   def index
     @players = Player.order(score: :desc).first(20)
+
+    d = Date.today
+    @weekly_players = PlayerHistory.where('created_at >= ? and created_at <= ?', d.at_beginning_of_week, d.at_beginning_of_week + 7.days).order(score: :desc).first(20)
+
   end
 
   def player
